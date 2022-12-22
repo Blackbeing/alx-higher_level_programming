a435841831b280ade504b15ddb08e34f32690716
Blackbeing <jonnieey55@gmail.com>
2022-12-22 01:18:06 +0000

    feat Define an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class definition for ALX assignments
+
+Defines class objects to be used in assignments.
+"""
+
+
+class Square:
+    """Square class
+
+    An empty class
+    """
+    pass
