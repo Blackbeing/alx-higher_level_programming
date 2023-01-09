f0ec442622baa03b50abab5bf212d0b7c4a69bcd
Blackbeing <jonnieey55@gmail.com>
2023-01-09 20:21:38 +0000

    refactor Correction is_same_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,15 @@ True
 
 
 def is_same_class(obj, a_class):
+    """
+    Test if object belongs to a class
+
+    Args:
+        obj: object
+        a_class: Class
+    Return:
+        True or false
+    """
     if type(obj) == a_class:
         return True
     return False
