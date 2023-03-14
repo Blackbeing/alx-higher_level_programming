78231d47667313a2b06bbcb6cc251fbaac6ede63
Blackbeing <johnjahi55@gmail.com>
2023-03-14 23:54:38 +0000

    feat Print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myString = '';
+      for (let j = 0; j < this.width; j++) {
+        myString += 'X';
+      }
+      console.log(myString);
+    }
+  }
+}
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -3,3 +3,4 @@
 - 0-rectangle.js -- Empty class with constructor
 - 1-rectangle.js -- Add class attributes
 - 2-rectangle.js -- Return empty object in construcor
+- 3-rectangle.js -- Print Rectangle using "X"
