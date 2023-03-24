c5fdbec35795f7a1ef4956f5a562af47f1e4b23d
Blackbeing <johnjahi55@gmail.com>
2023-03-24 02:16:20 +0000

    fix Correction

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+This module creates and SQLAlchemy engine,
+fetches states from specified database and prints them
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import Session
-from sqlalchemy import select, create_engine
+from sqlalchemy import select, create_engine, asc
 
 if __name__ == "__main__":
     user, passwd, db = sys.argv[1:4]
@@ -13,8 +17,8 @@ if __name__ == "__main__":
     )
     Base.metadata.create_all(engine)
 
-    stmt = select(State.name)
+    stmt = select(State.id, State.name).order_by(asc(State.id))
     with Session(engine) as session:
         states = session.execute(stmt).all()
-        for idx, state in enumerate(states, start=1):
-            print(f"{idx}: {state[0]}")
+        for state in states:
+            print(f"{state[0]}: {state[1]}")
