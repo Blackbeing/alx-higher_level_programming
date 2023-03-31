580eead3ccdab8e587d964fcfa03dfba846e4cb1
Blackbeing <johnjahi55@gmail.com>
2023-03-31 20:26:00 +0000

    feat Get url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides a script that fetches url and output its contents
+"""
+import urllib.request as request
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        print("Body response:")
+        content = res.read1()
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,2 @@
+# Python - Network #1
+- 0-hbtn_status.py -- Get url using urllib
